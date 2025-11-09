f6567116a5aff8cf6153cd801b802732fe19d084
mmprotest <simon.villani@gmail.com>
2025-11-09 12:28:16 +1100

    Handle fenced diffs with CRLF endings

--- tests/test_diffs.py
+++ tests/test_diffs.py
@@ -9,6 +9,16 @@ def test_parse_diff_single_hunk():
     assert hunks[0].replace == "bar"
 
 
+def test_parse_diff_multiple_hunks_with_fence_and_crlf():
+    diff_text = """```\r\n<<<<<<< SEARCH\r\nfoo\r\n=======\r\nbar\r\n>>>>>>> REPLACE\r\n\r\n<<<<<<< SEARCH\r\nspam\r\n=======\r\neggs\r\n>>>>>>> REPLACE\r\n```"""
+    hunks = parse_diff(diff_text)
+    assert len(hunks) == 2
+    assert hunks[0].search == "foo"
+    assert hunks[0].replace == "bar"
+    assert hunks[1].search == "spam"
+    assert hunks[1].replace == "eggs"
+
+
 def test_apply_diff():
     source = "foo = 1\n"
     hunk = DiffHunk(search="foo", replace="bar")
